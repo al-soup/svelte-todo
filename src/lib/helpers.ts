82eda30d214f360f089c6133130f6526658642aa
Alex Kräuchi <al@soup.one>
2022-10-18 15:56:11 +0200

    Add Id to todo items

---
+++ src/lib/helpers.ts
@@ -0,0 +1,2 @@
+export const createId = () =>
+  Date.now().toString(36) + Math.random().toString(36).substring(2);
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,12 @@
 import { writable } from "svelte/store";
+import { createId } from "./lib/helpers";
 
 export interface Todo {
-  id?: string; // TODO remove by ID
+  id: string; // TODO remove by ID
   done: boolean;
   task: string;
 }
 
-export const todos = writable([{ done: true, task: "First Task" }] as Todo[]);
+export const todos = writable([
+  { id: createId(), done: true, task: "First Task" },
+] as Todo[]);
